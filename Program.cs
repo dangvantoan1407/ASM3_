c9a30719a2fb48a3e339af15ff091ada171b81f1
dangvantoan1407 <toandvth2209025@fpt.edu.vn>
2023-08-17 15:26:10 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using ASM3.asm3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BankAccount account = new BankAccount();
+        account.BalanceChanged += new BankAccount.BalanceChangedEventHandler(HandleBalanceChanged);
+
+
+        account.Deposit(1000);
+        account.Withdraw(500);
+
+
+    }
+    static void HandleBalanceChanged(double newBalance)
+    {
+        Console.WriteLine($"Balance changed. New balance: {newBalance}");
+    }
+}
---
+++ asm3/BankAccount.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ASM3.asm3.BankAccount;
+
+namespace ASM3.asm3
+{
+    public delegate void BalanceChangedEventHandler( double newBalance);
+
+    public class BankAccount
+    {
+     
+        private double balance;
+       public event BalanceChangedEventHandler BalanceChanged;
+
+        public double Balance
+        {
+            get => balance; 
+            private set
+            {
+                if (balance != value)
+                {
+                    balance = value;
+                   
+                }
+            }
+        }
+        public void Withdraw(double amout)
+        {
+            if(amout > 0 && amout <= balance)
+            {
+                balance -= amout;
+                BalanceChanged(amout);
+            }
+        }
+        public void Deposit(double amount)
+        {
+            if(amount > 0)
+            {
+                balance += amount;
+                BalanceChanged(amount);
+            }
+        }
+
+
+        internal class BalanceChangedEventHandler
+        {
+            private Action<double> handleBalanceChanged;
+
+            public BalanceChangedEventHandler(Action<double> handleBalanceChanged)
+            {
+                this.handleBalanceChanged = handleBalanceChanged;
+            }
+        }
+    }
+
+
+}
